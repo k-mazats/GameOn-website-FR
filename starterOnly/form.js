29eeb7c0b5451f3daabfb66b0bdf59bce17696a3
Kevin Mazats <k.mazats@gmail.com>
2021-02-03 18:33:37 +0100

    issue #2 (birthdate?)

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -2,32 +2,43 @@
 
 const form = document.querySelector("form");
 
+// const errorMessages = {
+
+// }
+
 //inputs validations
 
+// first name
 function firstValidation() {
 	let inputValue = document.getElementById("first").value;
 	if (inputValue !== null && inputValue.length > 2) return true;
 	else return false;
 }
 
+// last name
 function lastValidation() {
 	let inputValue = document.getElementById("last").value;
 	if (inputValue !== null && inputValue.length > 2) return true;
 	else return false;
 }
 
+
+//email
 function emailValidation() {
 	let regex = /^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})$/;
 	let inputValue = document.getElementById("email").value;
 	return regex.test(inputValue);
 }
 
+
+// quantity
 function quantityValidation() {
 	let regex = /^[0-9]+$/;
 	let inputValue = document.getElementById("quantity").value;
 	return regex.test(inputValue);
 }
 
+// localisation
 function locationValidation() {
 	let radioButtons = document.querySelectorAll(".checkbox-input[type=radio]");
 	for (let radio of radioButtons) {
@@ -36,14 +47,14 @@ function locationValidation() {
 	return false;
 }
 
+//checkbox
 function checkboxValidation() {
 	let inputValue = document.getElementById("checkbox1").checked;
 	return inputValue;
 }
 
-document
-	.getElementById("button")
-	.addEventListener("click", function formValidation(event) {
+// global form validation
+function validate(event) {
 		event.preventDefault();
 		let isValid = true;
 		if (!firstValidation()) {
@@ -62,4 +73,4 @@ document
 		if (isValid) {
 			form.submit();
 		}
-	});
+	};
