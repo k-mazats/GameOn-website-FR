5349b5b87e6b188ccdb728ae8df7cf8e5519ffc9
Kevin Mazats <k.mazats@gmail.com>
2021-02-10 14:46:05 +0100

    better mail regex
    https://emailregex.com/

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -81,7 +81,7 @@ function lastValidation() {
 
 //check if email use valid formatting
 function emailValidation() {
-	let regex = /^([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})$/;
+	let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return regex.test(emailInput.value);
 }
 
