3c55e8a655c516812eab23cb784f1aae2f94b648
Kevin Mazats <k.mazats@gmail.com>
2021-02-17 15:51:02 +0100

    simpler mail regex

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -81,7 +81,7 @@ function lastValidation() {
 
 //check if email use valid formatting
 function emailValidation() {
-	let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	let regex = /^\S+@\S+\.\S+$/;
 	return regex.test(emailInput.value);
 }
 
